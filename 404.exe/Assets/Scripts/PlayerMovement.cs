a0b53c5da93efb296bcad435019ff2ce22275507
Arithoas <65888376+Arithoas@users.noreply.github.com>
2020-12-04 14:52:06 +0800

    Player movement and tap swipe

--- 404.exe/Assets/Scripts/PlayerMovement.cs
+++ 404.exe/Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float mSpeed = 28f;
+    private float initialSpeed;
     private Animator animator;
     public bool isDead;
 
     void PlayerRunning()
     {
         transform.Translate(Vector3.back * mSpeed * Time.deltaTime);
-        transform.Translate(Vector3.down * 3 * Time.deltaTime);
+        //transform.Translate(Vector3.down * 3 * Time.deltaTime);
     }
 
     // Start is called before the first frame update
     void Start()
     {
         animator = this.GetComponent<Animator>();
+        initialSpeed = mSpeed;
         isDead = false;
     }
     
@@ -28,5 +30,9 @@ public class PlayerMovement : MonoBehaviour
         {
             PlayerRunning();
         }
+
+
+        mSpeed = initialSpeed + (transform.position.z * 0.001f);
+
     }
 }
